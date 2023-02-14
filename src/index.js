156dd8acf0face71f098fa0bc2dd2c2b36a95a87
mafe cardenas <maria.cardenas.r3@gmail.com>
2023-02-14 13:20:21 -0300

    styling with classes (1/3)

--- src/index.js
+++ src/index.js
@@ -92,25 +92,76 @@ import ReactDOM from 'react-dom/client';
 
 // --------------------------PARENT/CHILD COMPONENTS (PART 2)----------------------------------------------
 
+// function Header() {
+//   return (
+//       <header>
+//           <nav>
+//               <img src="./react-logo.png" width="40px" />
+//           </nav>
+//       </header>
+//   )
+// }
+
+// function Footer(){
+//   return (
+//       <footer>
+//           <small>© 2021 Ziroll development. All rights reserved.</small>
+//       </footer>
+//   )
+// }
+// function MainContent(){
+//   return (
+//       <div>
+//           <h1>Reasons I'm excited to learn React</h1>
+//           <ol>
+//               <li>It's a popular library, so I'll be 
+//               able to fit in with the cool kids!</li>
+//               <li>I'm more likely to get a job as a developer
+//               if I know React</li>
+//           </ol>
+//       </div>
+//   )
+// }
+
+// function Page() {
+//   return (
+//       <div>
+//           <Header />
+//           <MainContent />
+//           <Footer />
+//       </div>
+//   )
+// }
+
+// ReactDOM.render(<Page />, document.getElementById("root"))
+
+// --------------------------STYLING WITH CLASSES (PART 1/3)----------------------------------------------
+
 
 function Header() {
   return (
       <header>
           <nav>
               <img src="./react-logo.png" width="40px" />
+              <ul>
+                  <li>Princing</li>
+                  <li>About</li>
+                  <li>Contact</li>
+              </ul>
           </nav>
       </header>
   )
 }
 
-function Footer(){
+function Footer() {
   return (
       <footer>
           <small>© 2021 Ziroll development. All rights reserved.</small>
       </footer>
   )
 }
-function MainContent(){
+
+function MainContent() {
   return (
       <div>
           <h1>Reasons I'm excited to learn React</h1>
