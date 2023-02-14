4ff35c63c90f635131e34bc0f6cc020072ec7735
mafe cardenas <maria.cardenas.r3@gmail.com>
2023-02-14 13:34:03 -0300

    Organize components

--- src/index.js
+++ src/index.js
@@ -1,5 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import Header from "./Header"
+import Footer from "./Footer"
+import MainContent from "./MainContent"
 
 //---------------PROJECT PART 1-----------------------------------------------------------------
 // const page = (
@@ -239,42 +242,57 @@ import ReactDOM from 'react-dom/client';
 
 // --------------------------STYLING WITH CLASSES (PART 3/3)----------------------------------------------
 
-function Header() {
-  return (
-      <header>
-          <nav className="nav">
-              <img src="./react-logo.png" className="nav-image"/>
-              <ul className="nav-items">
-                  <li>Pricing</li>
-                  <li>About</li>
-                  <li>Contact</li>
-              </ul>
-          </nav>
-      </header>
-  )
-}
+// function Header() {
+//   return (
+//       <header>
+//           <nav className="nav">
+//               <img src="./react-logo.png" className="nav-image"/>
+//               <ul className="nav-items">
+//                   <li>Pricing</li>
+//                   <li>About</li>
+//                   <li>Contact</li>
+//               </ul>
+//           </nav>
+//       </header>
+//   )
+// }
 
-function Footer() {
-  return (
-      <footer>
-          <small>© 2021 Ziroll development. All rights reserved.</small>
-      </footer>
-  )
-}
+// function Footer() {
+//   return (
+//       <footer>
+//           <small>© 2021 Ziroll development. All rights reserved.</small>
+//       </footer>
+//   )
+// }
 
-function MainContent() {
-  return (
-      <div>
-          <h1>Reasons I'm excited to learn React</h1>
-          <ol>
-              <li>It's a popular library, so I'll be 
-              able to fit in with the cool kids!</li>
-              <li>I'm more likely to get a job as a developer
-              if I know React</li>
-          </ol>
-      </div>
-  )
-}
+// // // function MainContent() {
+// // //   return (
+// // //       <div>
+// // //           <h1>Reasons I'm excited to learn React</h1>
+// // //           <ol>
+// // //               <li>It's a popular library, so I'll be 
+// // //               able to fit in with the cool kids!</li>
+// // //               <li>I'm more likely to get a job as a developer
+// // //               if I know React</li>
+// // //           </ol>
+// // //       </div>
+// // //   )
+// // // }
+
+// // function Page() {
+// //   return (
+// //       <div>
+// //           <Header />
+// //           <MainContent />
+// //           <Footer />
+// //       </div>
+// //   )
+// // }
+
+// ReactDOM.render(<Page />, document.getElementById("root"))
+
+// --------------------------ORGANIZE COMPONENTS----------------------------------------------
+
 
 function Page() {
   return (
