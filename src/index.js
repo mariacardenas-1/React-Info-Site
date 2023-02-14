895ba671c376e6f31827a72bcd76562e7f7d8911
mafe cardenas <maria.cardenas.r3@gmail.com>
2023-02-14 13:28:37 -0300

    styling with classes (3/3)

--- src/index.js
+++ src/index.js
@@ -186,12 +186,64 @@ import ReactDOM from 'react-dom/client';
 
 // ReactDOM.render(<Page />, document.getElementById("root"))
 
+// --------------------------STYLING WITH CLASSES (PART 2/3)----------------------------------------------
+
+// // // // // function Header() {
+// // // // //   return (
+// // // // //       <header>
+// // // // //           <nav className="nav">
+// // // // //               <img src="./react-logo.png" width="40px" />
+// // // // //               <ul className="nav-items">
+// // // // //                   <li>Pricing</li>
+// // // // //                   <li>About</li>
+// // // // //                   <li>Contact</li>
+// // // // //               </ul>
+// // // // //           </nav>
+// // // // //       </header>
+// // // // //   )
+// // // // // }
+
+// // // // function Footer() {
+// // // //   return (
+// // // //       <footer>
+// // // //           <small>© 2021 Ziroll development. All rights reserved.</small>
+// // // //       </footer>
+// // // //   )
+// // // // }
+
+// // // function MainContent() {
+// // //   return (
+// // //       <div>
+// // //           <h1>Reasons I'm excited to learn React</h1>
+// // //           <ol>
+// // //               <li>It's a popular library, so I'll be 
+// // //               able to fit in with the cool kids!</li>
+// // //               <li>I'm more likely to get a job as a developer
+// // //               if I know React</li>
+// // //           </ol>
+// // //       </div>
+// // //   )
+// // // }
+
+// // function Page() {
+// //   return (
+// //       <div>
+// //           <Header />
+// //           <MainContent />
+// //           <Footer />
+// //       </div>
+// //   )
+// // }
+
+// ReactDOM.render(<Page />, document.getElementById("root"))
+
+// --------------------------STYLING WITH CLASSES (PART 3/3)----------------------------------------------
 
 function Header() {
   return (
       <header>
           <nav className="nav">
-              <img src="./react-logo.png" width="40px" />
+              <img src="./react-logo.png" className="nav-image"/>
               <ul className="nav-items">
                   <li>Pricing</li>
                   <li>About</li>
