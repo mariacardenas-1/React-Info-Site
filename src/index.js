445b6a3f5954f2c55bab0e635682ff68c1d8d6dd
mafe cardenas <maria.cardenas.r3@gmail.com>
2023-02-14 13:25:21 -0300

    styling with classes (2.1/3)

--- src/index.js
+++ src/index.js
@@ -137,14 +137,63 @@ import ReactDOM from 'react-dom/client';
 
 // --------------------------STYLING WITH CLASSES (PART 1/3)----------------------------------------------
 
+// function Header() {
+//     return (
+//         <header>
+//             <nav>
+//                 <img src="./react-logo.png" width="40px" />
+//                 <ul>
+//                     <li>Princing</li>
+//                     <li>About</li>
+//                     <li>Contact</li>
+//                 </ul>
+//             </nav>
+//         </header>
+//     )
+// }
+
+// function Footer() {
+//     return (
+//         <footer>
+//             <small>© 2021 Ziroll development. All rights reserved.</small>
+//         </footer>
+//     )
+// }
+
+// function MainContent() {
+//     return (
+//         <div>
+//             <h1>Reasons I'm excited to learn React</h1>
+//             <ol>
+//                 <li>It's a popular library, so I'll be 
+//                 able to fit in with the cool kids!</li>
+//                 <li>I'm more likely to get a job as a developer
+//                 if I know React</li>
+//             </ol>
+//         </div>
+//     )
+// }
+
+// function Page() {
+//     return (
+//         <div>
+//             <Header />
+//             <MainContent />
+//             <Footer />
+//         </div>
+//     )
+// }
+
+// ReactDOM.render(<Page />, document.getElementById("root"))
+
 
 function Header() {
   return (
       <header>
-          <nav>
+          <nav className="nav">
               <img src="./react-logo.png" width="40px" />
-              <ul>
-                  <li>Princing</li>
+              <ul className="nav-items">
+                  <li>Pricing</li>
                   <li>About</li>
                   <li>Contact</li>
               </ul>
